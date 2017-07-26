50d63c18770dfb0e675d00a8aa686f6dacbaaf88
Trent Willis <twillis@netflix.com>
2017-07-26 09:11:31 -0700

    Log error messages from test run

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const inBrowserTest = require('./src/in-browser-test.js');
 QUnit.test.inBrowser = function inBrowser(description, url, test) {
   QUnit.test(description, async function(assert) {
     const result = await inBrowserTest(url, test);
+
+    const errors = result.tests[0].errors;
+    if (errors.length) {
+      errors.forEach((error) => console.log(error.message));
+    }
+
     assert.equal(result.status, 'passed');
   });
 };
